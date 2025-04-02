9dc0c91aadc8bc595bccab508301f9c620751e31
jayychaniyara <chaniyarajay007@gmail.com>
2025-04-02 23:24:33 +0530

    revert changes

--- DS.Frontend/vite.config.ts
+++ DS.Frontend/vite.config.ts
@@ -3,8 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-const isGitHubPages = process.env.NODE_ENV === "production";
-
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -12,16 +11,12 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  base: isGitHubPages ? "/decentralized-file-storage/" : "/", // Fixes GitHub Pages
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
   },
 }));
