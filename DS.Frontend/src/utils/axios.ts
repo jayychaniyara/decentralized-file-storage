503f6765655686e54c949309e986f7c14b927ef2
jayychaniyara <chaniyarajay007@gmail.com>
2025-05-15 23:29:25 +0530

    Backend URL Integrated

--- DS.Frontend/src/utils/axios.ts
+++ DS.Frontend/src/utils/axios.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
-});
+const baseURL =
+  import.meta.env.MODE === "production"
+    ? "https://blockstore-backend.onrender.com" 
+    : import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 
+const instance = axios.create({
+  baseURL,
+});
 export default instance;
